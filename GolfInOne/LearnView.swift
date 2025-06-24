6cb837421bbb7865b89c62c79b6ea93bc0a52b6e
JudgeDev <judge.py@gmail.con>
2025-06-24 23:59:19 +0200

    Add MainView icons and empty LearnView

---
+++ GolfInOne/LearnView.swift
@@ -0,0 +1,18 @@
+//
+//  LearnView.swift
+//  GolfInOne
+//
+//  Created by William Chandler on 24.06.25.
+//
+
+import SwiftUI
+
+struct LearnView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LearnView()
+}
